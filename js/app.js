ac31e62147f2f2919cd532e0f69132861b8c8855
Renato Miguel <renato.miguel@blip.pt>
2015-05-28 23:07:06 +0100

    Added initial stylesheets and scripts.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+requirejs.config({
+  'baseUrl': 'js/lib'
+  },
+  'paths': {
+    'app': '../app',
+
+    'jquery': '//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min'
+  }
+});
+
+requirejs(['app/main']);
